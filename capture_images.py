3332fdbbbcc6386e5eadfefc6a9a166aeab15aae
Simon Andrews <simon@proteo.me.uk>
2024-03-12 21:48:08 +0000

    Added some bigger structure to the script

--- capture_images.py
+++ capture_images.py
@@ -4,57 +4,106 @@ import time
 import numpy
 from PIL import Image, ImageFont, ImageDraw
 import datetime
+from pathlib import Path
+
+data_folder = Path("/home/andrewss/Windmill_Images")
+
+def main():
+    # We loop forever
+    while True:
+        # Figure out how many minutes it is until the next sunset
+        mins_to_sunset = get_mins_to_sunset()
+
+        # Create a folder for today
+        todays_folder = create_folder_for_today()
+
+        # Capture images
+        capture_images(mins_to_sunset,todays_folder)
+
+        # Create the video from todays images
+        create_video(todays_folder)
+
+        # Sleep until sunrise
+        sleep_until_sunrise()
 
 
-for i in range(600):
-    cam = cv2.VideoCapture(0)
+def get_mins_to_sunset():
+    return 10
 
-    cam.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
-    cam.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
 
-    # We don't want the camera to try to autofocus otherwise it 
-    # sometimes ends up focussing on the window instead of the mill
+def create_folder_for_today():
+    folder_name = datetime.datetime.now().strftime("%Y_%m_%d")
+    folder = data_folder / folder_name
 
-    cam.set(cv2.CAP_PROP_AUTOFOCUS,0)
-    cam.set(cv2.CAP_PROP_FOCUS, 0)
+    if not folder.exists():
+        folder.mkdir()
 
-    # When the camera first starts it sometimes takes a while for 
-    # the auto-exposure to settle down, so we need to wait for it
-    # to stablise 
+    return folder
 
-    time.sleep(5)
+def create_video(folder):
+    pass
 
-    # Now we can capture the image
-    result,image = cam.read()
+def sleep_until_sunrise():
+    pass
 
-    # We can shut the camera down now
-    cam.release()
 
-    image = cv2.rotate(image,cv2.ROTATE_90_COUNTERCLOCKWISE)
+def capture_images(minutes,folder):
+    for i in range(minutes):
+        cam = cv2.VideoCapture(0)
 
-    # Convert to PIL image for adding font
-    image = Image.fromarray(image)
+        cam.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
+        cam.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
 
-    # Get a drawing context so we can add the time to the image
-    draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf",32)
+        # We tried setting these values to disable autofocus but they
+        # weren't accepted by the gstreamer backend.  We appear to do
+        # the same thing using:
+        # sudo v4l2-ctl -c focus_automatic_continuous=0
+        # via v4l but who knows if that works.
+        # It does seem to stick though
+        # cam.set(cv2.CAP_PROP_AUTOFOCUS,0)
+        # cam.set(cv2.CAP_PROP_FOCUS, 0)
 
-    # Get the time in a form suitable for writing to the image
-    date = datetime.datetime.now().strftime("%a %d %b %Y %H:%M.%S")
+        # When the camera first starts it sometimes takes a while for 
+        # the auto-exposure to settle down, so we need to wait for it
+        # to stablise 
 
-    # Get the time in a form suitable for generating a filename
-    filename = datetime.datetime.now().strftime("%Y_%m_%d_%H.%M.%S.png")
+        time.sleep(5)
 
-    # Add the text to the image in a fixed width font
-    draw.text((20,1230),date,(255,255,255), font=font)
+        # Now we can capture the image
+        result,image = cam.read()
 
-    # Convert back to a cv2 object
-    image = numpy.array(image)
+        # We can shut the camera down now
+        cam.release()
 
-    # Write the file
-    cv2.imwrite(filename,image)
+        image = cv2.rotate(image,cv2.ROTATE_90_COUNTERCLOCKWISE)
 
-    # Wait for the next timepoint.  The whole cycle takes about a minute
-    time.sleep(50)
-    
-    
+        # Convert to PIL image for adding font
+        image = Image.fromarray(image)
+
+        # Get a drawing context so we can add the time to the image
+        draw = ImageDraw.Draw(image)
+        font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf",32)
+
+        # Get the time in a form suitable for writing to the image
+        date = datetime.datetime.now().strftime("%a %d %b %Y %H:%M.%S")
+
+        # Get the time in a form suitable for generating a filename
+        filename = datetime.datetime.now().strftime("%Y_%m_%d_%H.%M.%S.png")
+
+        # Add the text to the image in a fixed width font
+        draw.text((20,1230),date,(255,255,255), font=font)
+
+        # Convert back to a cv2 object
+        image = numpy.array(image)
+
+        # Write the file
+
+        filename = str(folder)+"/"+filename
+
+        cv2.imwrite(filename,image)
+
+        # Wait for the next timepoint.  The whole cycle takes about a minute
+        time.sleep(50)
+        
+if __name__ == "__main__":
+    main()
