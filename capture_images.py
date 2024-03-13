bb5dbc725db5a34a184d3e5c6779ba7200e518c1
Simon Andrews <simon@proteo.me.uk>
2024-03-13 19:20:09 +0000

    Leave camera on.  Make video. Extend times

--- capture_images.py
+++ capture_images.py
@@ -5,8 +5,9 @@ import numpy
 from PIL import Image, ImageFont, ImageDraw
 import datetime
 from pathlib import Path
-from suntime import Sun, SunTimeException
+from suntime import Sun
 from datetime import datetime,timedelta, timezone
+import subprocess
 
 data_folder = Path("/home/andrewss/Windmill_Images")
 # We use this for getting sunrise and sunset times
@@ -14,7 +15,8 @@ sun = Sun(52.1951,0.1313)
 
 
 def main():
-    # We loop forever
+
+    # We loop forever so we can collect for multiple days
     while True:
         # Figure out how many minutes it is until the next sunset
         sunset_time = get_sunset_time()
@@ -28,16 +30,21 @@ def main():
         # Create the video from todays images
         create_video(todays_folder)
 
+        # Upload to youtube?
+
         # Sleep until sunrise
         sleep_until_sunrise()
 
 
 def get_sunset_time():
-    return sun.get_sunset_time()
+    # We don't want the actual sunset time, we'll go 30 mins
+    # beyond that so we get a video which fades to black
+
+    return sun.get_sunset_time()+timedelta(minutes=30)
 
 
 def create_folder_for_today():
-    folder_name = datetime.datetime.now().strftime("%Y_%m_%d")
+    folder_name = datetime.now().strftime("%Y_%m_%d")
     folder = data_folder / folder_name
 
     if not folder.exists():
@@ -46,46 +53,66 @@ def create_folder_for_today():
     return folder
 
 def create_video(folder):
-    pass
+    # ffmpeg -framerate 15 -pattern_type glob -i '*.png' -c:v libx264 -vf fps=15 -pix_fmt yuv420p 2024_03_13.mp4
+
+    # Make an output file name from the folder name
+    output_file = folder.name + ".mp4"
+
+    if (folder / output_file).exists():
+        # The video has already been made for today
+        return
+
+    subprocess.run(["ffmpeg", "-framerate", "15", "-pattern_type", "glob", "-i", "*.png", "-c:v", "libx264", "-vf", "fps=15", "-pix_fmt yuv420p", output_file], cwd=folder)
 
 def sleep_until_sunrise():
     tomorrow_sunrise = sun.get_sunrise_time(datetime.today()+timedelta(days=1))
-    time_to_wait = tomorrow_sunrise - datetime.now(timezone.utc)
-    time.sleep(time_to_wait.total_seconds())
+    time_to_wait = (tomorrow_sunrise - datetime.now(timezone.utc)).total_seconds()
+
+    # We want to start the video a bit before the sunrise so we take off 30 mins
+    time_to_wait -= (60*30)
+
+    time.sleep(time_to_wait)
 
 
 
 def capture_images(time_until,folder):
+
+    # Rather than restart the camera for each shot (once a minute)
+    # we'll leave it running.  Hopefully that will give better
+    # consistency in the photos
+
+    cam = cv2.VideoCapture(0)
+
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
+
+    # We tried setting these values to disable autofocus but they
+    # weren't accepted by the gstreamer backend.  We appear to do
+    # the same thing using:
+    # sudo v4l2-ctl -c focus_automatic_continuous=0
+    # via v4l but who knows if that works.
+    # It does seem to stick though
+    # cam.set(cv2.CAP_PROP_AUTOFOCUS,0)
+    # cam.set(cv2.CAP_PROP_FOCUS, 0)
+
+    # When the camera first starts it sometimes takes a while for 
+    # the auto-exposure to settle down, so we need to wait for it
+    # to stablise 
+
+    time.sleep(5)
+
+    # We'll capture and throw away a few images as these also seem
+    # to help the camera to settle
+    for _ in range(5):
+        cam.read()
+
     while True:
 
         if datetime.now(timezone.utc) > time_until:
             break
 
-        cam = cv2.VideoCapture(0)
-
-        cam.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
-        cam.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
-
-        # We tried setting these values to disable autofocus but they
-        # weren't accepted by the gstreamer backend.  We appear to do
-        # the same thing using:
-        # sudo v4l2-ctl -c focus_automatic_continuous=0
-        # via v4l but who knows if that works.
-        # It does seem to stick though
-        # cam.set(cv2.CAP_PROP_AUTOFOCUS,0)
-        # cam.set(cv2.CAP_PROP_FOCUS, 0)
-
-        # When the camera first starts it sometimes takes a while for 
-        # the auto-exposure to settle down, so we need to wait for it
-        # to stablise 
-
-        time.sleep(5)
-
         # Now we can capture the image
         result,image = cam.read()
-
-        # We can shut the camera down now
-        cam.release()
 
         image = cv2.rotate(image,cv2.ROTATE_90_COUNTERCLOCKWISE)
 
@@ -97,10 +124,10 @@ def capture_images(time_until,folder):
         font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeMono.ttf",32)
 
         # Get the time in a form suitable for writing to the image
-        date = datetime.datetime.now().strftime("%a %d %b %Y %H:%M.%S")
+        date = datetime.now().strftime("%a %d %b %Y %H:%M.%S")
 
         # Get the time in a form suitable for generating a filename
-        filename = datetime.datetime.now().strftime("%Y_%m_%d_%H.%M.%S.png")
+        filename = datetime.now().strftime("%Y_%m_%d_%H.%M.%S.png")
 
         # Add the text to the image in a fixed width font
         draw.text((20,1230),date,(255,255,255), font=font)
@@ -116,6 +143,11 @@ def capture_images(time_until,folder):
 
         # Wait for the next timepoint.  The whole cycle takes about a minute
         time.sleep(50)
+
+
+    # When we're finished we shut the camera down
+    cam.release()
+
         
 if __name__ == "__main__":
     main()
